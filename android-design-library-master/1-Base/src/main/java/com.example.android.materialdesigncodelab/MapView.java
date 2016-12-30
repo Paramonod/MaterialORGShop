8a72181bfa0d8bf84c6af7a78f06c3d320fb836c
Paramonod <gamiysikdima@gmail.com>
2016-12-30 13:05:52 +0600

    FirstMaterial

---
+++ android-design-library-master/1-Base/src/main/java/com.example.android.materialdesigncodelab/MapView.java
@@ -0,0 +1,7 @@
+package com.example.android.materialdesigncodelab;
+
+/**
+ * Created by Dmitry Paramonov on 12/29/2016.
+ */
+public class MapView {
+}
---
+++ android-design-library-master/app/src/main/java/com/example/android/materialdesigncodelab/MapViewFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.materialdesigncodelab;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dmitry Paramonov on 12/29/2016.
+ */
+public class MapViewFragment extends android.support.v4.app.Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.map_view, container, false);
+    }
+}
